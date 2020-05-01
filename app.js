f382a35e4baf1f2d6b0fa66edd0f0643118c1872
WhiTE._.BeAR <62128227+WhiTEBeAR-99@users.noreply.github.com>
2020-05-01 13:23:59 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 // const hostname = 'localhost';
 // const port = 6002;
 
-app.use(express.json())
-app.use(express.urlencoded({
-    extended: true
-}))
-app.use(express.static('public'))
+// var path = require('path')
+// res.sendFile(path.join(__dirname, '../public', 'index1.html'));
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.static("public"));
 
 app.use((req, res, next) => {
-    console.log("request");
-    next();
+  console.log("request");
+  next();
 });
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html');
-})
+app.get("/", (req, res) => {
+  // res.sendFile('index.html');
+  res.sendFile("public/index.html", { root: __dirname });
+});
 
 var mangaRoute = require("./routers/mangasRouter");
-app.use('/mangas', mangaRoute);
+app.use("/mangas", mangaRoute);
 
 app.listen(port, hostname, () => {
-    console.log(`Hello! I am running at ${hostname}:${port}/`);
+  console.log(`Hello! I am running at ${hostname}:${port}/`);
 });
